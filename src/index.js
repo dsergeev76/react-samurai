166dd3b94ae671d2b445006d58c64a163d690fd1
Dmitiy <dsergeev@inbox.ru>
2020-11-18 21:02:19 +0300

    React. Way of Samurai. 35. Переделан вызов rerenderEntireTree в State.js и index.js на основе паттерна Observer (Наблюдатель) Render.js убран.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import * as serviceWorker from './serviceWorker';
+import state, {subscribe} from "./redux/State";
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import state from "./redux/State";
-import {addPost} from "./redux/State";
-import {rerenderEntireTree} from "./Render";
+import {addMessage, addPost, updateNewMessageText, updateNewPostText} from "./redux/State";
+
+let rerenderEntireTree = (state) => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText} addMessage={addMessage} updateNewMessageText={updateNewMessageText}/>
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
 
 rerenderEntireTree(state);
+
+subscribe(rerenderEntireTree); // паттерн "Наблюдатель" ("Observer")
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/State.js
+++ src/redux/State.js
@@ -1,4 +1,6 @@
-import {rerenderEntireTree} from "../Render";
+let rerenderEntireTree = () => {
+    console.log('State changed');
+}
 
 let state = {
 
@@ -32,7 +34,7 @@ let state = {
 
 }
 
-export let addPost = () => {
+export const addPost = () => {
     let newPost = {
         id: 3,
         post: state.profilePage.newPostText,
@@ -43,12 +45,12 @@ export let addPost = () => {
     rerenderEntireTree(state);
 }
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
     state.profilePage.newPostText = newText;
     rerenderEntireTree(state);
 }
 
-export let addMessage = () => {
+export const addMessage = () => {
     let newMessage = {
         id: 4,
         message: state.dialogsPage.newMessageText
@@ -58,9 +60,13 @@ export let addMessage = () => {
     rerenderEntireTree(state);
 }
 
-export let updateNewMessageText = (newText) => {
+export const updateNewMessageText = (newText) => {
     state.dialogsPage.newMessageText = newText;
     rerenderEntireTree(state);
+}
+
+export const subscribe = (observer) => { // паттерн "Наблюдатель" ("Observer")
+    rerenderEntireTree = observer;
 }
 
 export default state;
