1785b7074cedd22928e268ac477b23606844c817
Dmitiy <dsergeev@inbox.ru>
2020-11-20 18:03:18 +0300

    React. Way of Samurai. 37. Изменение в State.js - создан объект Store. Изменения в index.js для работы с объектом Store

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import * as serviceWorker from './serviceWorker';
-import state, {subscribe} from "./redux/State";
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {addMessage, addPost, updateNewMessageText, updateNewPostText} from "./redux/State";
+import store from "./redux/State";
 
 let rerenderEntireTree = (state) => {
     ReactDOM.render(
         <React.StrictMode>
-            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText} addMessage={addMessage} updateNewMessageText={updateNewMessageText}/>
+            <App state={store.getState()} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)} addMessage={store.addMessage.bind(store)} updateNewMessageText={store.updateNewMessageText.bind(store)}/>
         </React.StrictMode>,
         document.getElementById('root')
     );
 }
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree); // паттерн "Наблюдатель" ("Observer")
+store.subscribe(rerenderEntireTree); // паттерн "Наблюдатель" ("Observer")
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/State.js
+++ src/redux/State.js
@@ -1,72 +1,75 @@
-let rerenderEntireTree = () => {
-    console.log('State changed');
-}
+let store = {
+    _state: {
 
-let state = {
+        profilePage: {
+            posts: [
+                {id: 1, post: "Привет, как дела?", likes: 30},
+                {id: 2, post: "Мое первое тестовое сообщение", likes: 80}
+            ],
+            newPostText: ''
+        },
 
-    profilePage: {
-        posts: [
-            {id: 1, post: "Привет, как дела?", likes: 30},
-            {id: 2, post: "Мое первое тестовое сообщение", likes: 80}
-        ],
-        newPostText: ''
+        dialogsPage: {
+            dialogs: [
+                {id: 1, name: "Степан Чугунов"},
+                {id: 2, name: "Аполлинарий Котлованов"},
+                {id: 3, name: "Владимир Пинтусевич"},
+                {id: 4, name: "Анатолий Спаснев"},
+                {id: 5, name: "Дмитрий Лукин"}
+            ],
+            messages: [
+                {id: 1, message: "Привет!"},
+                {id: 2, message: "Как дела?"},
+                {id: 3, message: "Хорошо. А у тебя?"}
+            ],
+            newMessageText: ''
+        },
+
+        sideBar: {
+
+        }
+
+    },
+    getState () {
+      return this._state
+    },
+    _callSubscriber() {
+        console.log('State changed');
+    },
+    addPost() {
+        let newPost = {
+            id: 3,
+            post: this._state.profilePage.newPostText,
+            likes: 0
+        }
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText (newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    addMessage () {
+        let newMessage = {
+            id: 4,
+            message: this._state.dialogsPage.newMessageText
+        }
+        this._state.dialogsPage.messages.push(newMessage);
+        this._state.dialogsPage.newMessageText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewMessageText (newText) {
+        this._state.dialogsPage.newMessageText = newText;
+        this._callSubscriber(this._state);
     },
 
-    dialogsPage: {
-        dialogs: [
-            {id: 1, name: "Степан Чугунов"},
-            {id: 2, name: "Аполлинарий Котлованов"},
-            {id: 3, name: "Владимир Пинтусевич"},
-            {id: 4, name: "Анатолий Спаснев"},
-            {id: 5, name: "Дмитрий Лукин"}
-        ],
-        messages: [
-            {id: 1, message: "Привет!"},
-            {id: 2, message: "Как дела?"},
-            {id: 3, message: "Хорошо. А у тебя?"}
-        ],
-        newMessageText: ''
-    },
-
-    sideBar: {
-
+    subscribe (observer) { // паттерн "Наблюдатель" ("Observer")
+        this._callSubscriber = observer;
     }
 
 }
 
-export const addPost = () => {
-    let newPost = {
-        id: 3,
-        post: state.profilePage.newPostText,
-        likes: 0
-    }
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
-}
+export default store;
 
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-}
-
-export const addMessage = () => {
-    let newMessage = {
-        id: 4,
-        message: state.dialogsPage.newMessageText
-    }
-    state.dialogsPage.messages.push(newMessage);
-    state.dialogsPage.newMessageText = '';
-    rerenderEntireTree(state);
-}
-
-export const updateNewMessageText = (newText) => {
-    state.dialogsPage.newMessageText = newText;
-    rerenderEntireTree(state);
-}
-
-export const subscribe = (observer) => { // паттерн "Наблюдатель" ("Observer")
-    rerenderEntireTree = observer;
-}
-
-export default state;
+window.store = store;
