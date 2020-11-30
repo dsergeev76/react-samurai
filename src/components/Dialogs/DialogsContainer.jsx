62338f0fb9a3d213a68427a277ba5a05c7701f6e
Dmitiy <dsergeev@inbox.ru>
2020-11-30 17:00:03 +0300

    React. Way of Samurai. 45. Изменена передача Store в компоненты MyPostsContainer.jsx и DialogsContainer.jsx с Context на использование connect, mapStateToProps, mapDispatchToProps из библиотеки React-Redux.

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -1,42 +1,26 @@
 import React from "react";
 import {addMessageActionCreator, updateNewMessageTextActionCreator} from "../../redux/dialogsReducer";
 import Dialogs from "./Dialogs";
-import StoreContext from "../../StoreContext";
+import {connect} from "react-redux";
 
-const DialogsContainer = (props) => {
-
-    /*    let state = props.store.getState();
-
-        let addMessage = () => {
-            props.store.dispatch(addMessageActionCreator());
-        }
-
-        let onMessageChange = (body) => {
-            let action = updateNewMessageTextActionCreator(body);
-            props.store.dispatch(action);
-        }*/
-
-    return (
-        <StoreContext.Consumer>
-            { (store) => {
-
-                let state = store.getState();
-
-                let addMessage = () => {
-                    store.dispatch(addMessageActionCreator());
-                }
-
-                let onMessageChange = (body) => {
-                    let action = updateNewMessageTextActionCreator(body);
-                    store.dispatch(action);
-                }
-
-                return <Dialogs updateNewMessageBody={onMessageChange} addMessage={addMessage} dialogsPage={state.dialogsPage}/>
-            }
-            }
-        </StoreContext.Consumer>
-    )
+let mapStateToProps = (state) => {
+    return {
+        dialogsPage: state.dialogsPage
+    }
 }
 
+let mapDispatchToProps = (dispatch) => {
+    return {
+        updateNewMessageBody:(body) => {
+            let action = updateNewMessageTextActionCreator(body);
+            dispatch(action);
+        },
+        addMessage:() => {
+            dispatch(addMessageActionCreator());
+        }
+    }
+}
+
+const DialogsContainer = connect (mapStateToProps, mapDispatchToProps)(Dialogs);
 
 export default DialogsContainer;
--- src/index.js
+++ src/index.js
@@ -4,14 +4,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import store from "./redux/redux-store";
-import StoreContext from "./StoreContext";
+import {Provider} from "react-redux";
+
 
 let rerenderEntireTree = (state) => {
     ReactDOM.render(
         <React.StrictMode>
-            <StoreContext.Provider value={store}>
+            <Provider store={store}>
                 <App />
-            </StoreContext.Provider>
+            </Provider>
         </React.StrictMode>,
         document.getElementById('root')
     );
