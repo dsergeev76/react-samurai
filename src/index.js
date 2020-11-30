081e3f83afe9e5e52201abfdd75d51a4803c300a
Dmitiy <dsergeev@inbox.ru>
2020-11-30 18:19:59 +0300

    React. Way of Samurai. 47. Изменения в profileReducer.js и dialogsReducer.js - вместо state производится возврат копии state.

--- src/index.js
+++ src/index.js
@@ -6,23 +6,14 @@ import App from './App';
 import store from "./redux/redux-store";
 import {Provider} from "react-redux";
 
-
-let rerenderEntireTree = (state) => {
-    ReactDOM.render(
-        <React.StrictMode>
-            <Provider store={store}>
-                <App />
-            </Provider>
-        </React.StrictMode>,
-        document.getElementById('root')
-    );
-}
-
-rerenderEntireTree(store.getState());
-
-store.subscribe(() =>{  // паттерн "Наблюдатель" ("Observer")
-    let state = store.getState();
-    rerenderEntireTree (state)});
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
