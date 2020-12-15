36e2675071290ac0ae0b14b77674bffc95b1d0d0
Dmitiy <dsergeev@inbox.ru>
2020-12-15 15:55:09 +0300

    React. Way of Samurai. 51. Реализовано получение списка пользователей с сервера - запрос с использованием axios методом get.

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import styles from "./users.module.css";
+import * as axios from "axios";
+import userPhoto from "../../assets/images/user.jpg";
 
 const Users = (props) => {
 
     if (props.users.length === 0) {
-        props.setUsers(
+
+        axios.get ("https://social-network.samuraijs.com/api/1.0/users").then(response =>{
+            props.setUsers(response.data.items);
+        });
+
+        /*props.setUsers(
             [
                 {id:1, photoURL:'https://a.d-cd.net/96fc986s-960.jpg', followed: false, fullName: 'Аполлинарий Котлованов', status: 'Начальник абзетцера. Проводим работы нулевого цикла на заказ', location: {city:'Lopatino', country:'Russia'}},
                 {id:2, photoURL:'https://cs9.pikabu.ru/post_img/2016/09/20/9/1474380038136283489.jpg', followed: true, fullName: 'Владимир Пинтусевич', status: 'M113 - is history...', location: {city:'Petrovich', country:'Everon'}},
                 {id:3, photoURL:'https://arma-cwa.ru/wp-content/uploads/2015/10/Piloty-SSSR.png', followed: true, fullName: 'Анатолий Спаснев', status: 'Взлетел я, значит, с Колгуева...', location: {city:'Airbase', country:'Kolguev'}}
             ]
-        )
+        )*/
     }
 
     return <div>
@@ -18,7 +25,7 @@ const Users = (props) => {
             props.users.map(user =><div key={user.id}>
                 <span>
                     <div>
-                        <img src={user.photoURL} alt="Фото" className={styles.userPhoto}/>
+                        <img src={user.photos.small != null ? user.photos.small : userPhoto} alt="Фото" className={styles.userPhoto}/>
                     </div>
                     <div>
                         {user.followed ? <button onClick={() => {props.unfollow(user.id)} }>Unfollow</button> : <button onClick={() => {props.follow(user.id)} }>Follow</button>}
@@ -26,12 +33,12 @@ const Users = (props) => {
                 </span>
                 <span>
                     <span>
-                        <div>{user.fullName}</div>
+                        <div>{user.name}</div>
                         <div>{user.status}</div>
                     </span>
                     <span>
-                        <div>{user.location.country}</div>
-                        <div>{user.location.city}</div>
+                        <div>{"user.location.country"}</div>
+                        <div>{"user.location.city"}</div>
                     </span>
                 </span>
                 </div>
