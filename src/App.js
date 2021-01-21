05e715433401104cd8f547e3989946f42bee2917
Dmitiy <dsergeev@inbox.ru>
2021-01-21 20:13:18 +0300

    React. Way of Samurai. 60. Реализована загрузка и отображение профиля пользователя, выбранного из списка в разделе "Пользователи". Чтение ID пользователя из текущего URL с помощью withRouter.

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App(props) {
               <Header/>
               <Navbar/>
               <div className="app-wrapper-content">
-                  <Route path="/Profile" render={ () => <ProfileContainer />}/>
+                  <Route path="/Profile/:userId?" render={ () => <ProfileContainer />}/>
                   <Route path="/Dialogs" render={ () => <DialogsContainer />}/>
                   <Route path="/Users" render={ () => <UsersContainer />}/>
                   <Route path="/News" render={ () => <News />}/>
