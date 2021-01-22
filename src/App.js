970bfb9a635b4487211186ebe8640ad9a372a4d2
Dmitiy <dsergeev@inbox.ru>
2021-01-22 20:31:30 +0300

    React. Way of Samurai. 61. Изменения в Header.jsx - в правый угол заголовка добавлена ссылка на авторизацию, либо вывод текущего имени авторизованного пользователя. При загрузке происходит AJAX-запрос к серверу для получения данных пользователя.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import News from "./components/News/News";
@@ -9,12 +8,13 @@ import Settings from "./components/Settings/Settings";
 import {Route, BrowserRouter} from "react-router-dom";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileInfo/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 function App(props) {
   return (
       <BrowserRouter>
           <div className="app-wrapper">
-              <Header/>
+              <HeaderContainer/>
               <Navbar/>
               <div className="app-wrapper-content">
                   <Route path="/Profile/:userId?" render={ () => <ProfileContainer />}/>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import style from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
 const Header = (props) => {
     return <header className={style.header}>
         <img
             src="http://ae01.alicdn.com/kf/H1f4b6861a02346e792321ba949b40ceby/Tuning-Cars-Stickers-3D-Vinyl-Car-Wrap-For-Auto-Goods-Decals-Accessories-Products-Styling-The-Army.jpg_220x220q90.jpg"
             alt="logo"/>
+            <div className={style.loginBlock}>
+                {props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink>}
+            </div>
     </header>;
 }
 export default Header;
