8839fec55217abb0a47b01c81697c97697955a1d
Dmitiy <dsergeev@inbox.ru>
2021-01-23 17:55:22 +0300

    React. Way of Samurai. 62. В списке пользователей добавлена подписка (follow и unfollow) на других пользователей соцсети с передачей данных на сервер и получением ответа от него.

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./users.module.css";
 import userPhoto from "../../assets/images/user.jpg";
 import {NavLink} from "react-router-dom";
+import * as axios from "axios";
 
 let Users = (props) => {
 
@@ -28,7 +29,33 @@ let Users = (props) => {
                         </div>
                     </NavLink>
                     <div>
-                        {user.followed ? <button onClick={() => {props.unfollow(user.id)} }>Unfollow</button> : <button onClick={() => {props.follow(user.id)} }>Follow</button>}
+                        {user.followed ?
+                            <button onClick={() => {
+                                axios.delete (`https://social-network.samuraijs.com/api/1.0/follow/${user.id}`, {
+                                    withCredentials: true,
+                                    headers: {
+                                        "API-KEY": "dd9c9b22-2524-4727-9b12-db8ac64e1aff"
+                                    }
+                                })
+                                    .then(response => {
+                                        if (response.data.resultCode === 0) {
+                                            props.unfollow(user.id)
+                                        }
+                                    });
+                            }}>Unfollow</button>:
+                            <button onClick={() => {
+                                axios.post (`https://social-network.samuraijs.com/api/1.0/follow/${user.id}`, {}, {
+                                    withCredentials: true,
+                                    headers: {
+                                        "API-KEY": "dd9c9b22-2524-4727-9b12-db8ac64e1aff"
+                                    }
+                                })
+                                    .then(response => {
+                                    if (response.data.resultCode === 0) {
+                                        props.follow(user.id)
+                                    }
+                                });
+                            }}>Follow</button>}
                     </div>
                 </span>
                     <span>
