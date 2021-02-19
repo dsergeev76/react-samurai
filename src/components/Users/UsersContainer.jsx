5f66410cfb154ad4a56cefc29728b73136d6cf9e
Dmitiy <dsergeev@inbox.ru>
2021-02-19 18:54:42 +0300

    React. Way of Samurai. 81. В UsersContainer.jsx mapStateToProps переделан на использование селекторов из созданного users-selectors.js

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -9,6 +9,14 @@ import {
 } from "../../redux/users-reducer";
 import Users from "./Users";
 import Preloader from "../common/Preloader/Preloader";
+import {
+    getCurrentPage,
+    getFollowingInProgress,
+    getIsFetching,
+    getPageSize,
+    getTotalUsersCount,
+    getUsersSelector
+} from "../../redux/users-selectors";
 
 class UsersContainer extends React.Component {
 
@@ -39,6 +47,17 @@ class UsersContainer extends React.Component {
 
 let mapStateToProps = (state) => {
     return {
+        users: getUsersSelector(state),
+        pageSize: getPageSize(state),
+        totalUsersCount: getTotalUsersCount(state),
+        currentPage: getCurrentPage(state),
+        isFetching: getIsFetching(state),
+        followingInProgress: getFollowingInProgress(state)
+    }
+}
+
+/*let mapStateToProps = (state) => {
+    return {
         users: state.usersPage.users,
         pageSize: state.usersPage.pageSize,
         totalUsersCount: state.usersPage.totalUsersCount,
@@ -46,7 +65,7 @@ let mapStateToProps = (state) => {
         isFetching: state.usersPage.isFetching,
         followingInProgress: state.usersPage.followingInProgress
     }
-}
+}*/
 
 /*let mapDispatchToProps = (dispatch) => {
     return {
