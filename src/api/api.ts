a3d3f67857c9b79b55e5c5c6411b4800192e9465
Dmitiy <dsergeev@inbox.ru>
2021-03-29 21:27:47 +0300

    React. Way of Samurai 2.0. 07. Внедрение TypeScript. Проведена типизация API (Me и Login).

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,5 @@
-import * as axios from "axios";
+import axios from "axios";
+import {ProfileType} from "../types/types";
 
 const instance = axios.create ({
     withCredentials:true,
@@ -17,21 +18,21 @@ export const usersAPI = {
             })
     },
 
-    follow (userId) {
+    follow (userId: number) {
         return instance.post ('follow/'+userId,{})
             .then(response => {
                 return response.data;
             })
     },
 
-    unfollow (userId) {
+    unfollow (userId: number) {
         return instance.delete ('follow/'+userId)
             .then(response => {
                 return response.data;
             })
     },
 
-    getProfile (userId) {
+    getProfile (userId: number) {
         console.warn('Obsolete method. Use profileAPI.getProfile')
         return profileAPI.getProfile(userId);
     }
@@ -40,34 +41,61 @@ export const usersAPI = {
 
 export const profileAPI = {
 
-    getProfile (userId) {
+    getProfile (userId: number) {
         return instance.get (`profile/`+userId);
     },
-    getStatus (userId) {
+    getStatus (userId: any) {
         return instance.get (`profile/status/`+userId);
     },
-    updateStatus (statusText) {
+    updateStatus (statusText: string) {
         return instance.put ('profile/status/', {status:statusText});
     },
-    savePhoto (photoFile) {
+    savePhoto (photoFile: any) {
         const formData = new FormData();
         formData.append("image", photoFile)
         return instance.put ('profile/photo', formData, {
-            'Content-type': 'multipart/form-data'
+            headers: {'Content-type': 'multipart/form-data'}
         });
     },
-    saveProfile (profile) {
+    saveProfile (profile: ProfileType) {
         return instance.put ('profile', profile);
     }
 }
 
+export enum ResultCodesEnum {
+    Success = 0,
+    Error = 1
+}
+
+export enum ResultCodeForCaptchaEnum {
+    CaptchaIsRequired = 10
+}
+
+type MeResponseType = {
+    data: {
+        id: number
+        email: string
+        login: string
+    }
+    resultCode: ResultCodesEnum
+    messages: Array<string>
+}
+
+type LoginResponseType = {
+    data: {
+        userId: number
+    }
+    resultCode: ResultCodesEnum | ResultCodeForCaptchaEnum
+    messages: Array<string>
+}
+
 export const authAPI = {
     me () {
-        return instance.get (`auth/me`);
+        return instance.get<MeResponseType>(`auth/me`).then(response => response.data);
     },
 
-    login (email, password, rememberMe = false, captcha = null) {
-        return instance.post ('auth/login',{email:email, password:password, rememberMe: rememberMe, captcha: captcha});
+    login (email: string, password: string, rememberMe = false, captcha: string | null = null) {
+        return instance.post<LoginResponseType> ('auth/login',{email:email, password:password, rememberMe: rememberMe, captcha: captcha}).then(response => response.data);
     },
 
     logout () {
