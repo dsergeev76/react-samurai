fe41ec485149c0b443e01df32abc3e8b54586222
Dmitiy <dsergeev@inbox.ru>
2021-04-23 21:14:33 +0300

    React. Way of Samurai 2.0. 17. Внедрено использование библиотеки стилей Ant Design

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
-import Navbar from "./components/Navbar/Navbar";
+import 'antd/dist/antd.css';
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-import {BrowserRouter, Redirect, Route, Switch, withRouter} from "react-router-dom";
+import {BrowserRouter, Link, Redirect, Route, Switch, withRouter} from "react-router-dom";
 import {UsersPage} from "./components/Users/UsersContainer";
-import HeaderContainer from "./components/Header/HeaderContainer";
 import {connect, Provider} from "react-redux";
 import {initializeApp} from "./redux/app-reducer";
 import Preloader from "./components/common/Preloader/Preloader";
@@ -16,6 +15,13 @@ import {withSuspense} from "./hoc/withSuspense";
 import {LoginPage} from "./components/Login/LoginPage";
 //import DialogsContainer from "./components/Dialogs/DialogsContainer";
 //import ProfileContainer from "./components/Profile/ProfileInfo/ProfileContainer";
+import {Breadcrumb, Layout, Menu} from 'antd';
+import {LaptopOutlined, NotificationOutlined, UserOutlined} from '@ant-design/icons';
+import {Header} from "./components/Header/Header";
+
+const {SubMenu} = Menu;
+const {Content, Footer, Sider} = Layout;
+
 const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
 const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileInfo/ProfileContainer'));
 
@@ -43,12 +49,66 @@ class App extends React.Component<MapPropsType & DispatchPropsType> {
     }
 
     render() {
-        if (!this.props.initialized){
+        if (!this.props.initialized) {
             return <Preloader/>
+
         }
 
         return (
-            <div className="app-wrapper">
+            <Layout>
+                <Header/>
+                <Content style={{padding: '0 50px'}}>
+                    <Breadcrumb style={{margin: '16px 0'}}>
+                        <Breadcrumb.Item>Home</Breadcrumb.Item>
+                        <Breadcrumb.Item>List</Breadcrumb.Item>
+                        <Breadcrumb.Item>App</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <Layout className="site-layout-background" style={{padding: '24px 0'}}>
+                        <Sider className="site-layout-background" width={200}>
+                            <Menu
+                                mode="inline"
+                                //defaultSelectedKeys={['1']}
+                                //defaultOpenKeys={['sub1']}
+                                style={{height: '100%'}}
+                            >
+                                <SubMenu key="sub1" icon={<UserOutlined/>} title="Мой профиль">
+                                    <Menu.Item key="1"><Link to="/Profile">Профиль</Link></Menu.Item>
+                                    <Menu.Item key="2"><Link to="/Dialogs">Сообщения</Link></Menu.Item>
+                                    <Menu.Item key="3">option3</Menu.Item>
+                                    <Menu.Item key="4">option4</Menu.Item>
+                                </SubMenu>
+                                <SubMenu key="sub2" icon={<LaptopOutlined/>} title="Пользователи">
+                                    <Menu.Item key="5"><Link to="/Users">Пользователи</Link></Menu.Item>
+                                    <Menu.Item key="6">option6</Menu.Item>
+                                    <Menu.Item key="7">option7</Menu.Item>
+                                    <Menu.Item key="8">option8</Menu.Item>
+                                </SubMenu>
+                                <SubMenu key="sub3" icon={<NotificationOutlined/>} title="subnav 3">
+                                    <Menu.Item key="9">option9</Menu.Item>
+                                    <Menu.Item key="10">option10</Menu.Item>
+                                    <Menu.Item key="11">option11</Menu.Item>
+                                    <Menu.Item key="12">option12</Menu.Item>
+                                </SubMenu>
+                            </Menu>
+                        </Sider>
+                        <Content style={{padding: '0 24px', minHeight: 280}}>
+                            <Switch>
+                                <Route exact path="/" render={() => <Redirect to={"/Profile/"}/>}/>
+                                <Route path="/Profile/:userId?" render={() => <SuspendedProfile/>}/>
+                                <Route path="/Dialogs" render={() => <SuspendedDialogs/>}/>
+                                <Route path="/Users" render={() => <UsersPage pageTitle={'Самураи'}/>}/>
+                                <Route path="/News" render={() => <News/>}/>
+                                <Route path="/Music" render={() => <Music/>}/>
+                                <Route path="/Settings" render={() => <Settings/>}/>
+                                <Route path="/Login" render={() => <LoginPage/>}/>
+                                <Route path="*" render={() => <div>404 - NOT FOUND</div>}/>
+                            </Switch>
+                        </Content>
+                    </Layout>
+                </Content>
+                <Footer style={{textAlign: 'center'}}>Samurai Social Network ©2021 Created by IT-KAMASUTRA</Footer>
+            </Layout>
+            /*<div className="app-wrapper">
                 <HeaderContainer/>
                 <Navbar/>
                 <div className="app-wrapper-content">
@@ -64,7 +124,7 @@ class App extends React.Component<MapPropsType & DispatchPropsType> {
                         <Route path="*" render={() => <div>404 - NOT FOUND</div>}/>
                     </Switch>
                 </div>
-            </div>
+            </div>*/
         );
     }
 }
@@ -73,14 +133,14 @@ const mapStateToProps = (state: AppStateType) => ({
     initialized: state.app.initialized
 })
 
-let AppContainer = compose<React.ComponentType> (
+let AppContainer = compose<React.ComponentType>(
     withRouter,
-    connect (mapStateToProps, {initializeApp})) (App);
+    connect(mapStateToProps, {initializeApp}))(App);
 
 const SamuraiJSApp: React.FC = () => {
     return <BrowserRouter>
         <Provider store={store}>
-            <AppContainer />
+            <AppContainer/>
         </Provider>
     </BrowserRouter>
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,24 +1,56 @@
 import React from 'react';
-import style from './Header.module.css';
-import {NavLink} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
+import {Avatar, Button, Col, Layout, Menu, Row} from "antd";
+import {UserOutlined} from "@ant-design/icons";
+import {useDispatch, useSelector} from "react-redux";
+import {currentUserLogin, selectIsAuth} from "../../redux/auth-selectors";
+import {logout} from "../../redux/auth-reducer";
 
-export type MapPropsType = {
-    isAuth: boolean
-    login: string | null
-}
+export type MapPropsType = {}
 
-export type DispatchPropsType = {
-    logout: () => void
-}
+export const Header: React.FC<MapPropsType> = (props) => {
 
-const Header: React.FC<MapPropsType & DispatchPropsType> = (props) => {
-    return <header className={style.header}>
+    const isAuth = useSelector(selectIsAuth)
+    const login = useSelector(currentUserLogin)
+
+    const dispatch = useDispatch()
+
+    const logoutCallback = () => {
+        dispatch(logout())
+    }
+
+    const {Header} = Layout;
+
+    return <Header className="header">
+        <div className="logo"/>
+        <Row>
+            <Col span={18}>
+                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+                    <Menu.Item key="1"><NavLink to="/Users">Пользователи</NavLink></Menu.Item>
+                </Menu>
+            </Col>
+            {isAuth
+                ? <> <Col span={1}>
+                    <Avatar alt={login || ''} style={{backgroundColor: '#87d068'}} icon={<UserOutlined/>}/>
+                </Col>
+                    <Col span={5}>
+                        <Button onClick={logoutCallback}>Log out</Button>
+                    </Col>
+                </>
+                : <Col span={6}>
+                    <Button><Link to={'/login'}>Login</Link></Button>
+                </Col>
+            }
+        </Row>
+    </Header>
+
+
+    /*<header className={style.header}>
         <img
             src="http://ae01.alicdn.com/kf/H1f4b6861a02346e792321ba949b40ceby/Tuning-Cars-Stickers-3D-Vinyl-Car-Wrap-For-Auto-Goods-Decals-Accessories-Products-Styling-The-Army.jpg_220x220q90.jpg"
             alt="logo"/>
             <div className={style.loginBlock}>
                 {props.isAuth ? <div>{props.login} - <button onClick={props.logout}>Log out</button></div>: <NavLink to={'/login'}>Login</NavLink>}
             </div>
-    </header>;
+    </header>;*/
 }
-export default Header;
